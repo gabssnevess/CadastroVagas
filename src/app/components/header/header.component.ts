c3bbb3f4afe21361d1375ed45019be472b6174ff
Gabriel Neves <gabrielnevesoliveiraquintao@gmail.com>
2025-08-21 15:22:22 -0300

    Add HeaderComponent with user session management and update HomePage layout

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,56 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController, IonicModule } from '@ionic/angular';
+import { Supabase } from 'src/app/services/supabase/supabase';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class HeaderComponent implements OnInit {
+  constructor(private supabase: Supabase, private router: Router, private alertController: AlertController) {}
+  async ngOnInit() {
+    await this.getSession();
+    await this.getUser();
+  }
+
+  // Pegando o usuário logado
+  usuario: any;
+  async getUser() {
+    const {data, error} = await this.supabase.getUser();
+    if (error) {
+      // console.error('Erro ao pegar usuário:', error);
+      this.PresentAlert('🟡 Atenção!', 'Favor realizar login novamente.');
+      this.router.navigate(['/login']);
+    } else {
+      this.usuario = data.user.email?.split('@', 1);
+    }
+  }
+
+  // Pegando a sessão do usuário
+  async getSession() {
+    const { data, error } = await this.supabase.getSession();
+    return data.session ?? null;
+  }
+
+  // Logout
+  async logOut() {
+    await this.supabase.logOut();
+    this.router.navigate(['/login']);
+  }
+
+  // Criando alertas
+  async PresentAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+}
